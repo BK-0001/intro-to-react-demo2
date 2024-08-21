5401230432e54a01e45449e12002900e0be86a2f
Bart Kang <bart.kang.0719@gmail.com>
2024-08-21 11:14:03 -0700

    demo useState hook

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,14 +1,36 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function Form() {
+  // state is readonly
+  const [inputValue, setInputValue] = useState("");
+  // let inputValue = "";
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
+
+    const formData = {
+      email: inputValue
+    };
+
+    // send request to store data in the db
   };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value, "inside change handler");
+    setInputValue(() => event.target.value);
+  };
+
+  console.log(inputValue, "outside");
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="form-input">Input</label>
-      <input type="text" id="form-input" />
+      <input
+        value={inputValue}
+        onChange={handleChange}
+        type="text"
+        id="form-input"
+      />
       <button>Submit</button>
     </form>
   );
