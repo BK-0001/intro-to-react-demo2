ad066e01d63c998a7a7a4001c868705ad6a4f3bd
Bart Kang <bart.kang.0719@gmail.com>
2024-08-21 09:45:10 -0700

    demo multi buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { Button } from "./Button";
 
 function App() {
+  const handleClick = () => {};
+
+  const handleReset = () => {};
+
+  const handleNext = () => {};
+  const handlePrevious = () => {};
+
   return (
     <div>
-      <Button>children</Button>
+      <input type="text" />
+      <Button disabled={false} onClick={handleClick}>
+        Click Me!
+      </Button>
+      <Button onClick={handleReset}>
+        <div>Reset</div>
+      </Button>
+      <Button onClick={handlePrevious}>Previous Page</Button>
+      <Button onClick={handleNext}>Next Page</Button>
     </div>
   );
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,9 +1,15 @@
-import { ReactNode } from "react";
+import { MouseEvent, ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
+  disabled?: boolean;
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
 };
 
-export function Button({ children }: Props) {
-  return <button>{children}</button>;
+export function Button({ children, disabled = false, onClick }: Props) {
+  return (
+    <button disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
